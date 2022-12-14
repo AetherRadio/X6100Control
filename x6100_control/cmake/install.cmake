3d2413050440e9ebe8f583255124e6f27f64325d
Rui Oliveira <ruimail24@gmail.com>
2022-12-14 12:58:24 +0000

    Add a comment explaining how some variables work

--- x6100_control/cmake/install.cmake
+++ x6100_control/cmake/install.cmake
@@ -5,6 +5,8 @@ target_include_directories(aether_x6100_control #
 
 install(TARGETS aether_x6100_control)
 
+# The `CMAKE_CURRENT_[BINARY|SOURCE]_DIR` variables work because this file is `include`'d from the
+# main CMakeLists.txt
 install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/${AETHER_INCLUDE_DIR}
         DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
 
