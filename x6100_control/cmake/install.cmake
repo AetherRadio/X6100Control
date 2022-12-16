fea67209f6d433ba2149a9410308d5aa2eb338a8
Rui Oliveira <ruimail24@gmail.com>
2022-12-16 14:00:36 +0000

    Remove header file installation

--- x6100_control/cmake/install.cmake
+++ x6100_control/cmake/install.cmake
@@ -4,11 +4,3 @@ target_include_directories(aether_x6100_control #
                            PUBLIC $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>)
 
 install(TARGETS aether_x6100_control)
-
-# The `CMAKE_CURRENT_[BINARY|SOURCE]_DIR` variables work because this file is `include`'d from the
-# main CMakeLists.txt
-install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/${AETHER_INCLUDE_DIR}
-        DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
-
-install(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/${AETHER_INCLUDE_DIR}
-        DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
