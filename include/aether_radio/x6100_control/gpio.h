f7c66fd8d9148dda42728d3a77ccc1916320aa95
Rui Oliveira <ruimail24@gmail.com>
2022-12-18 10:19:47 +0000

    Use better macro and variable names

--- include/aether_radio/x6100_control/gpio.h
+++ include/aether_radio/x6100_control/gpio.h
@@ -14,9 +14,9 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-AETHER_X6100_CONTROL_API extern int x6100_pin_wifi;
-AETHER_X6100_CONTROL_API extern int x6100_pin_rf;
-AETHER_X6100_CONTROL_API extern int x6100_pin_light;
+AETHER_X6100CTRL_API extern int x6100_pin_wifi;
+AETHER_X6100CTRL_API extern int x6100_pin_rf;
+AETHER_X6100CTRL_API extern int x6100_pin_light;
 
-AETHER_X6100_CONTROL_API bool x6100_gpio_init();
-AETHER_X6100_CONTROL_API void x6100_gpio_set(int pin, int value);
+AETHER_X6100CTRL_API bool x6100_gpio_init();
+AETHER_X6100CTRL_API void x6100_gpio_set(int pin, int value);
