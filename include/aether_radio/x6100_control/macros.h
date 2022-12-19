65bc57fe44b37c9b2b009dda67a1b29bec3403a6
Rui Oliveira <ruimail24@gmail.com>
2022-12-19 15:04:52 +0000

    Add macros.h

---
+++ include/aether_radio/x6100_control/macros.h
@@ -0,0 +1,11 @@
+/*
+ *  SPDX-License-Identifier: LGPL-2.1-or-later
+ *
+ *  Aether Xiegu X6100 Control
+ *
+ *  Copyright (c) 2022 Rui Oliveira aka CT7ALW
+ */
+
+#pragma once
+
+#define AETHER_X6100CTRL_PACKED __attribute__((__packed__))
