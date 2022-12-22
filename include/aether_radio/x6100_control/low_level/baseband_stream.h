30a71f54b679ab338e5ca23c723fd3808066ddde
Rui Oliveira <ruimail24@gmail.com>
2022-12-22 14:38:59 +0000

    Minor touch ups on baseband_stream.h

--- include/aether_radio/x6100_control/low_level/baseband_stream.h
+++ include/aether_radio/x6100_control/low_level/baseband_stream.h
@@ -15,7 +15,8 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-#define AETHER_X6100CTRL_BB_IQ_SAMPLES_COUNT 512
+#define AETHER_X6100CTRL_BB_FRAME_IQ_SAMPLES_COUNT 512
+#define AETHER_X6100CTRL_BB_FRAME_MAGIC 0xAA5555AA
 
 #ifdef __cplusplus
 extern "C"
@@ -29,27 +30,36 @@ extern "C"
         bool chg : 1;
         bool vext : 1;
         uint32_t reserved : 28;
-    } aether_x6100ctrl_bb_stream_flags_t;
+    } aether_x6100ctrl_bb_frame_flags_t;
 
     typedef struct AETHER_X6100CTRL_PACKED
     {
-        uint32_t magic;
-        aether_x6100ctrl_fcomplex_t bb_iq_samples[AETHER_X6100CTRL_BB_IQ_SAMPLES_COUNT];
+        uint32_t magic; /*!< Every frame starts with AETHER_X6100CTRL_BB_FRAME_MAGIC */
 
-        aether_x6100ctrl_bb_stream_flags_t flags;
+        aether_x6100ctrl_fcomplex_t bb_iq_samples[AETHER_X6100CTRL_BB_FRAME_IQ_SAMPLES_COUNT];
+
+        aether_x6100ctrl_bb_frame_flags_t flags;
+
         uint8_t reserved_1;
+
         uint8_t tx_power;
         uint8_t vswr;
         uint8_t alc_level;
         uint8_t vext;
         uint8_t vbat;
         uint8_t batcap;
+
         uint8_t reserved_2;
+
         uint32_t atu_params;
-        uint32_t reserved_3[4];
+
+        uint32_t reserved_3;
+        uint32_t reserved_4;
+        uint32_t reserved_5;
+        uint32_t reserved_6;
 
         uint32_t crc;
-    } aether_x6100ctrl_bb_stream_t;
+    } aether_x6100ctrl_bb_frame_t;
 
 #ifdef __cplusplus
 }
