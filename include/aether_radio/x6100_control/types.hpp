327484fa5d615facd3869b188b09745ed7222d02
Rui Oliveira <ruimail24@gmail.com>
2023-01-09 23:20:34 +0000

    Refactor out the C++ typedefs to their own header file

---
+++ include/aether_radio/x6100_control/types.hpp
@@ -0,0 +1,18 @@
+/*
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ *
+ * Aether Xiegu X6100 Control
+ *
+ * SPDX-FileCopyrightText: 2022 Rui Oliveira <ruimail24@gmail.com>
+ */
+
+#pragma once
+
+#include "aether_radio/x6100_control/cpp_compat.h"
+
+namespace aether_radio::x6100ctrl::types
+{
+
+using fcomplex_t = aether_x6100ctrl_fcomplex_t;
+
+} // namespace aether_radio::x6100ctrl::types
