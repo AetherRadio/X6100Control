f693ba53925e265324ac2089f4fd09570b3ea771
Rui Oliveira <ruimail24@gmail.com>
2023-01-09 22:57:10 +0000

    Add macros to disable copy/move

--- include/aether_radio/x6100_control/macros.h
+++ include/aether_radio/x6100_control/macros.h
@@ -9,3 +9,18 @@
 #pragma once
 
 #define AETHER_X6100CTRL_PACKED __attribute__((__packed__))
+
+#ifdef __cplusplus
+
+    #define AETHER_X6100CTRL_DISABLE_COPY(some_class) \
+        some_class(const some_class &) = delete;      \
+        some_class &operator=(const some_class &) = delete;
+    #define AETHER_X6100CTRL_DISABLE_MOVE(some_class) \
+        some_class(some_class &&) = delete;           \
+        some_class &operator=(some_class &&) = delete;
+
+    #define AETHER_X6100CTRL_DISABLE_COPY_MOVE(some_class) \
+        AETHER_X6100CTRL_DISABLE_COPY(some_class)          \
+        AETHER_X6100CTRL_DISABLE_MOVE(some_class)
+
+#endif
